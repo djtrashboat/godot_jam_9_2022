a4e4b73a922e06dced00afbdaae18a60ef8a9775
trashbait <enricommandelli@gmail.com>
2022-09-09 15:40:06 -0300

    comentários (ainda n terminados)

--- platformer template/Player.gd
+++ platformer template/Player.gd
@@ -1,13 +1,13 @@
 extends KinematicBody2D
 
-onready var sprite = $AnimatedSprite
-onready var atk_cd = $atk_cd
-onready var effect = $braco/effect
+onready var sprite = $AnimatedSprite#animated sprite do player
+onready var atk_cd = $atk_cd#timer de cooldown entre tiros
+onready var effect = $braco/effect#efeito de raio quando o player atira
 
 var velocity = Vector2.ZERO
-var knockedout = false
-var can_shoot = true
-var is_recoil = false
+var knockedout = false#ativo após o player atirar no ar, ele perde o controle do personagem
+var can_shoot = true#ativo quando o player pode atirar: após o cd de cada tiro
+var is_recoil = false#ativo após o player atirar no chão, ele perde o controle do personagem por uma fração de segundo e sofre uma aceleração oposta a direção do tiro
 
 export var speed = Vector2(150,330)
 export var gravity = 12
@@ -26,28 +26,30 @@ func get_input_direction():
 	return Vector2(Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left"), -Input.get_action_strength("ui_up"))
 
 func calculate_velocity(direction: Vector2):
-	if is_recoil:
-		pass
+	if is_recoil:#logo após atirar, o player não tem controle sobre o player por uma fração de segundo
+		pass# a velocidade do player é contrária ao sentido do tiro e é calculada em shooting_recoil()
 	elif !is_on_floor():
 		if !knockedout:
-			velocity.x = lerp(velocity.x, direction.x * speed.x, 0.1)
+			velocity.x = lerp(velocity.x, direction.x * speed.x, 0.1)#está no ar e não knocado, tem controle, mas com aceleração em x (lerp com peso baixo)
 			if is_on_ceiling() or direction.y >= 0 and velocity.y<=0:
-				velocity.y = 0
+				velocity.y = 0#reseta a velocidade vertical quando o player bate no teto ou solta o botão de pular
 		elif is_on_ceiling():
-			velocity.y = 0
+			velocity.y = 0#reseta se o player bater no teto com o player knocado também
 		elif is_on_wall():
-			velocity.x *= -0.5
+			velocity.x *= -0.5#se estiver knocado e bater na parede, ele quica (bounce)
 		velocity.y += gravity
-	elif direction.y < 0:
-		knockedout = false
+	elif direction.y < 0:# no chão e pulo apertado
+		knockedout = false#ao pousar o player deixa de estar knocado
 		$braco/mao.visible = true
-		velocity.x = lerp(velocity.x, direction.x * speed.x, 0.6)
-		velocity.y = direction.y * speed.y
-	else:
-		knockedout = false
+		velocity.x = lerp(velocity.x, direction.x * speed.x, 0.6) #linha desnecessária#linha desnecessária
+		#as linhas acima só fazem tudo funcionar se o player não soltar o botão de pulo
+		#sem elas, buga se o player spamar o pulo sem soltar pq ele n chega a terminar de "pousar"
+		velocity.y = direction.y * speed.y #performa o pulo
+	else:#no chão
+		knockedout = false#ao pousar o player deixa de estar knocado
 		$braco/mao.visible = true
-		velocity.x = lerp(velocity.x, direction.x * speed.x, 0.6)
-		velocity.y = gravity
+		velocity.x = lerp(velocity.x, direction.x * speed.x, 0.6)#player controla o personagem normalmente com aceleração relativamente alta
+		velocity.y = gravity #para a velocidade vertical não ficar aumentando enquanto o player está no chão 
 
 #func animate():
 #	if knockedout:
